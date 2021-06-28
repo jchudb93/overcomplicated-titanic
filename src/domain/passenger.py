fcb6a240d8fcc847a36d826f4298f4963b901071
jchudb93 <jchudb93@gmail.com>
2021-06-27 20:57:31 -0500

    passenger dataclasses

--- src/domain/passenger.py
+++ src/domain/passenger.py
@@ -1,5 +1,23 @@
+import uuid
+import dataclasses
 
+@dataclasses.dataclass
 class Passenger:
-	
-	def __init__(self):
-		
+	code: uuid.UUID
+	passenger_id: int
+	survived: int
+	name: str
+	sex: str
+	age: int
+	sib_sp: int
+	parch: int
+	ticket: str
+	fare: float
+	cabin: str
+
+	@classmethod
+	def from_dict(self, d):
+		return self(**d)
+
+	def to_dict(self):
+		return dataclasses.asdict(self)
--- tests/domain/test_passenger.py
+++ tests/domain/test_passenger.py
@@ -3,29 +3,21 @@ from src.domain.passenger import Passenger
 
 
 def test_passenger_model_init():
-	code = uuid.uuid4
-	passenger = Passenger(
-		code = code,
-		passenger_id = 1,
-		survived = 0,
-		name = "Braund, Mr. Owen Harris",
-		sex = "male",
-		age = 22,
-		sib_sp = 0,
-		parch = 0,
-		ticket = "A/5 21171",
-		fare = 7.25,
-		cabin = ""
-	)
 
-	assert passenger.code == code
-	assert passenger.passenger_id == 1
-	assert passenger.survived == 0
-	assert passenger.name == "Braund, Mr. Owen Harris"
-	assert passenger.sex == "male"
-	assert passenger.age == 22
-	assert passenger.sib_sp == 0
-	assert passenger.parch == 0
-	assert passenger.ticket == "A/5 21171"
-	assert passenger.fare == 7.25
-	assert passenger.cabin == ""
+	init_dict = {
+		"code": uuid.uuid4,
+		"passenger_id": 1,
+		"survived": 0,
+		"name": "Braund, Mr. Owen Harris",
+		"sex": "male",
+		"age": 22,
+		"sib_sp": 0,
+		"parch": 0,
+		"ticket": "A/5 21171",
+		"fare": 7.25,
+		"cabin": ""
+	}
+
+	passenger = Passenger.from_dict(init_dict)
+
+	assert passenger.to_dict() == init_dict
